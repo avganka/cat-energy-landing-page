c8442b672ce2a4e668a12ae79043158dac387e51
“avganka” <karambasterg@gmail.com>
2022-03-15 07:49:26 +0300

    Реализовано открытие и закрытие мобильного меню

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 import '../sass/style.scss';
+
+
+//Mobile menu
+const btn = document.querySelector(".main-nav__toggle");
+const menu = document.querySelector(".main-nav__list");
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('main-nav__list--opened')
+})
